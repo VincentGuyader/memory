03da3bd727693c5b0ca04616e34b941bdddad9e3
Vincent Guyader <vincent@thinkr.fr>
2019-02-19 23:34:02 +0100

    unix version

--- R/app_server.R
+++ R/app_server.R
@@ -4,14 +4,26 @@
 #' @importFrom stats rnorm
 #' @importFrom utils capture.output memory.limit
 app_server <- function(input, output,session) {
-  # List the first level callModules here
+
+  
+  
+  # memfree <- as.numeric(system("awk '/MemFree/ {print $2}' /proc/meminfo",intern=TRUE))  
+  
   
   data <- reactiveValues(e=NULL)
   
   output$dt <- renderTable({
     invalidateLater(1000, session)
-    data.frame(memory = capture.output(pryr::mem_used() ),
-               memory.limit = prettyunits::pretty_bytes(memory.limit()*1000000)
+    data.frame(memory = capture.output(pryr::mem_used() )
+               ,
+               memory.limit = prettyunits::pretty_bytes(
+                 # memory.limit()
+                 as.numeric(system("awk '/MemTotal/ {print $2}' /proc/meminfo",intern=TRUE))  
+                 *10000),
+               memory.free = prettyunits::pretty_bytes(
+                 # memory.limit()
+                 as.numeric(system("awk '/MemFree/ {print $2}' /proc/meminfo",intern=TRUE))  
+                 *10000)
                
                )
     
@@ -19,10 +31,13 @@ app_server <- function(input, output,session) {
   
   observeEvent(input$more,{
     message("more")
+    message(capture.output(pryr::mem_used() ))
     data$e<-append(data$e,list(matrix(rnorm(10000000),ncol=10)))
   })
   observeEvent(input$less,{
     message("less")
+    message(capture.output(pryr::mem_used() ))
+    
     data$e <- data$e[-length(data$e)]
   })
   observeEvent(input$gc,{
